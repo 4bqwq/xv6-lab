e5e8ec2cfc0bc71c6d635947a65b3db65084dd90
4bqwq <18201860039@163.com>
2025-11-01 14:54:29 +0800

    feat(start): delegate traps to S-mode and call timer_init

--- src/kernel/boot/start.c
+++ src/kernel/boot/start.c
@@ -4,11 +4,22 @@
 __attribute__((aligned(16))) uint8 CPU_stack[4096 * NCPU];
 
 extern void main();
+extern void timer_init(void);
 
 void start()
 {
     // 暂时不开启分页，使用物理地址
     w_satp(0);
+
+    // 将能委托的异常与中断都委托给 S 模式
+    w_medeleg(~0ULL);
+    w_mideleg(~0ULL);
+
+    // 允许 S 模式访问 mcycle/minstret/mtime
+    w_mcounteren(0x7);
+
+    // 初始化定时器
+    timer_init();
 
     // 切换到S-mode后无法访问M-mode的寄存器
     // 所以需要将hartid存到可访问的寄存器tp
