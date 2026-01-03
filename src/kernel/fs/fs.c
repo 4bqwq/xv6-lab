35da87dd08eb007fd3a55ce990d1b24ea18ee791
4bqwq <18201860039@163.com>
2026-01-03 20:04:46 +0800

    debug(fs): add dentry lookup/create/delete test-3 in fs_init

--- src/kernel/fs/fs.c
+++ src/kernel/fs/fs.c
@@ -48,86 +48,76 @@ void fs_init()
     assert(sb.block_size == BLOCK_SIZE, "fs_init: unexpected block size");
     assert(sb.total_blocks > 0, "fs_init: total_blocks invalid");
 
-	/* fs_init in fs.c */
-
+    /* fs_init in fs.c */
 	printf("============= test begin =============\n\n");
 
-	inode_t *ip_1, *ip_2;
-	uint32 len, cut_len;
+	inode_t *rooti, *ip_1, *ip_2, *ip_3;
+	uint32 inode_num_1, inode_num_2, inode_num_3;
+	uint32 len, cutlen, offset;
+	char tmp[10];
 
-	/* 小批量读写测试 */
+	tmp[9] = 0;
+	cutlen = 9;
+	rooti = inode_get(ROOT_INODE);
 
-	int small_src[10], small_dst[10];
-	for (int i = 0; i < 10; i++)
-		small_src[i] = i;
-	
-	ip_1 = inode_create(INODE_TYPE_DATA, INODE_MAJOR_DEFAULT, INODE_MINOR_DEFAULT);
+	/* 搜索预置的dentry */
+
+	inode_lock(rooti);
+	inode_num_1 = dentry_search(rooti, "ABCD.txt");
+	inode_num_2 = dentry_search(rooti, "abcd.txt");
+	inode_num_3 = dentry_search(rooti, ".");
+	if (inode_num_1 == INVALID_INODE_NUM || 
+		inode_num_2 == INVALID_INODE_NUM || 
+		inode_num_3 == INVALID_INODE_NUM) {
+		panic("invalid inode num!");
+	}
+	dentry_print(rooti);
+	inode_unlock(rooti);
+
+	ip_1 = inode_get(inode_num_1);
 	inode_lock(ip_1);
-	inode_print(ip_1, "small_data");
-
-	printf("writing data...\n\n");
-	cut_len = 10 * sizeof(int);
-	for (uint32 offset = 0; offset < 400 * cut_len; offset += cut_len) {
-		len = inode_write_data(ip_1, offset, cut_len, small_src, false);
-		assert(len == cut_len, "write fail 1!");
-	}
-	inode_print(ip_1, "small_data");
-
-	len = inode_read_data(ip_1, 120 * cut_len + 4, cut_len, small_dst, false);
-	assert(len == cut_len, "read fail 1!");
-	printf("read data:");
-	for (int i = 0; i < 10; i++)
-		printf(" %d", small_dst[i]);
-	printf("\n\n");
-
-	ip_1->disk_info.nlink = 0;
-	inode_unlock(ip_1);
-	inode_put(ip_1);
-
-	/* 大批量读写测试 */
-
-	char *big_src, big_dst[9];
-	big_dst[8] = 0;
-
-	/* 申请五个连续物理页面 (初始化阶段, 通常来说能拿到连续的) */
-	big_src = pmem_alloc(true);
-	assert(pmem_alloc(true) == big_src + PGSIZE, "contiguous fail!");
-	assert(pmem_alloc(true) == big_src + PGSIZE * 2, "contiguous fail!");
-	assert(pmem_alloc(true) == big_src + PGSIZE * 3, "contiguous fail!");
-	assert(pmem_alloc(true) == big_src + PGSIZE * 4, "contiguous fail!");
-
-	for (uint32 i = 0; i < 5 * (PGSIZE / 8); i++)
-		for (uint32 j = 0; j < 8; j++)
-			big_src[i * 8 + j] = 'A' + j;
-
-	ip_2 = inode_create(INODE_TYPE_DATA, INODE_MAJOR_DEFAULT, INODE_MINOR_DEFAULT);
+	ip_2 = inode_get(inode_num_2);
 	inode_lock(ip_2);
-	inode_print(ip_2, "big_data");
+	ip_3 = inode_get(inode_num_3);
+	inode_lock(ip_3);
 
-	printf("writing data...\n\n");
-	cut_len = PGSIZE * 4 + 1110;
-	for (uint32 offset = 0; offset < cut_len * 150; offset += cut_len)
-	{
-		len = inode_write_data(ip_2, offset, cut_len, big_src, false);
-		assert(len == cut_len, "write fail 2!");
-	}
-	inode_print(ip_2, "big_data");
+	inode_print(ip_1, "ABCD.txt");
+	inode_print(ip_2, "abcd.txt");
+	inode_print(ip_3, "root");
 
-	len = inode_read_data(ip_1, cut_len * 150 - 8, 8, big_dst, false);
-	assert(len == 8, "read fail 2");
-	printf("read data: %s\n", big_dst);
+	len = inode_read_data(ip_1, 0, cutlen, tmp, false);
+	assert(len == cutlen, "read fail 1!");
+	printf("\nread data: %s\n", tmp);
 
+	len = inode_read_data(ip_2, 0, cutlen, tmp, false);
+	assert(len == cutlen, "read fail 2!");
+	printf("read data: %s\n\n", tmp);
 
-	ip_2->disk_info.nlink = 0;
+	inode_unlock(ip_1);
 	inode_unlock(ip_2);
+	inode_unlock(ip_3);
+	inode_put(ip_1);
 	inode_put(ip_2);
+	inode_put(ip_3);
 
-	pmem_free((uint64)big_src, true);
-	pmem_free((uint64)big_src + PGSIZE, true);
-	pmem_free((uint64)big_src + PGSIZE * 2, true);
-	pmem_free((uint64)big_src + PGSIZE * 3, true);
-	pmem_free((uint64)big_src + PGSIZE * 4, true);
+	/* 创建和删除dentry */
+	inode_lock(rooti);
 
+	ip_1 = inode_create(INODE_TYPE_DIR, INODE_MAJOR_DEFAULT, INODE_MINOR_DEFAULT);	
+	offset = dentry_create(rooti, ip_1->inode_num, "new_dir");
+	inode_num_1 = dentry_search(rooti, "new_dir");
+	printf("new dentry offset = %d\n", offset);
+	printf("new dentry inode_num = %d\n\n", inode_num_1);
+	
+	dentry_print(rooti);
+
+	inode_num_2 = dentry_delete(rooti, "new_dir");
+	assert(inode_num_1 == inode_num_2, "inode num is not equal!");
+
+	dentry_print(rooti);
+
+	inode_unlock(rooti);
+	inode_put(rooti);
 
 	printf("============= test end =============\n");
 
